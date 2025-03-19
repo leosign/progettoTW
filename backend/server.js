d6f6670af618e89bba77eb39785173b9c9ef0a51
leosign <leonardo.signorini@studio.unibo.it>
2025-03-19 17:34:11 +0100

    primo commit, struttura base del backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const cors = require ('cors');
+const mongoose = require ('mongoose');
+const dotenv = require ('dotenv');
+
+const app = express();
+app.use(cors());
+
